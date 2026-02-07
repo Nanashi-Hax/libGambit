4b3816df1b5ef42ac7e7a2b002b3cf1d3c7c0243
Nanashi-Hax <nanashi.rainbowhacker@gmail.com>
2026-02-08 03:58:47 +0900

    MainMgrBase::sInstance

--- Source/Gambit/Game/MainMgrBase.cpp
+++ Source/Gambit/Game/MainMgrBase.cpp
@@ -5,7 +5,7 @@ namespace Library::Gambit
 
 namespace Game
 {
-    MainMgrBase *const MainMgrBase::sInstance = reinterpret_cast<MainMgrBase *const>(0x106E5814);
+    MainMgrBase *const MainMgrBase::sInstance = *reinterpret_cast<MainMgrBase *const *>(0x106E5814);
 }
 
 }
