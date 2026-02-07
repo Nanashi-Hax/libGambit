8f54254ca670cd1de4a3597ee9554531bc8eb64e
Nanashi-Hax <nanashi.rainbowhacker@gmail.com>
2026-02-08 03:13:36 +0900

    Player::position

--- Public/Gambit/Game/Player.hpp
+++ Public/Gambit/Game/Player.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Gambit/sead/def.hpp"
 #include <Hook.hpp>
 #include <cstdint>
 
@@ -12,10 +13,13 @@ namespace Library::Gambit
         public:
             uint8_t padding0[0x24];
             int index;
+            uint8_t padding1[0x220];
+            sead::Vector3<float> position;
 
             METHOD(0x0E658BD0, void, startMotion_BombThrow, Player, int unk0, int unk1);
         };
 
         CHECK_OFFSET(Player, index, 0x24);
+        CHECK_OFFSET(Player, position, 0x248);
     }
 }
