26878421c0a394bcf81c3d1998f8d82c43432dd2
Nanashi-Hax <nanashi.rainbowhacker@gmail.com>
2026-02-08 03:10:18 +0900

    MainMgrBase::playerMgr

--- Public/Gambit/Game/MainMgrBase.hpp
+++ Public/Gambit/Game/MainMgrBase.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Gambit/Game/BulletMgr.hpp"
+#include "Gambit/Game/PlayerMgr.hpp"
 #include "Hook/Assert.hpp"
 #include <Hook.hpp>
 #include <cstdint>
@@ -13,7 +14,10 @@ namespace Game
     class MainMgrBase
     {
     public:
-        uint8_t padding0[0x274];
+        
+        uint8_t padding0[0x268];
+        PlayerMgr * playerMgr;
+        uint8_t padding1[0x8];
         BulletMgr * bulletMgr;
 
         static MainMgrBase *const sInstance;
@@ -21,6 +25,7 @@ namespace Game
         METHOD(0x0E2302D8, Lp::Sys::Actor *, activateOneCancelUnnecessary, BulletMgr, int id, bool unk0, sead::Vector3<float> * position, sead::Vector3<float> * velocity);
     };
 
+    CHECK_OFFSET(MainMgrBase, playerMgr, 0x268);
     CHECK_OFFSET(MainMgrBase, bulletMgr, 0x274);
 }
 
